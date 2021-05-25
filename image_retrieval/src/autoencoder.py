44900ece02a85d74f918b284c16834d72647e2f5
KimHeeSu linux <wlskaka4@gmail.com>
2021-05-25 21:36:40 +0900

    kim_linux first commit

--- image_retrieval/src/autoencoder.py
+++ image_retrieval/src/autoencoder.py
@@ -6,6 +6,7 @@
 import numpy as np
 import tensorflow as tf
 from src.utils import split
+import keras
 
 class AutoEncoder():
 
@@ -40,47 +41,47 @@ class AutoEncoder():
         if self.modelName == "simpleAE":
             encode_dim = 128
 
-            input = tf.keras.Input(shape=shape_img_flattened)
-            encoded = tf.keras.layers.Dense(encode_dim, activation='relu')(input)
+            input = keras.Input(shape=shape_img_flattened)
+            encoded = keras.layers.Dense(encode_dim, activation='relu')(input)
 
-            decoded = tf.keras.layers.Dense(shape_img_flattened[0], activation='sigmoid')(encoded)
+            decoded = keras.layers.Dense(shape_img_flattened[0], activation='sigmoid')(encoded)
 
         elif self.modelName == "convAE":
             n_hidden_1, n_hidden_2, n_hidden_3 = 16, 8, 8
             convkernel = (3, 3)  # convolution kernel
             poolkernel = (2, 2)  # pooling kernel
 
-            input = tf.keras.layers.Input(shape=shape_img) # (512, 512)
-            x = tf.keras.layers.Conv2D(n_hidden_1, convkernel, activation='relu', padding='same')(input) # (512, 512)
-            x = tf.keras.layers.MaxPooling2D(poolkernel, padding='same')(x)                              # (256, 256)
-            x = tf.keras.layers.Conv2D(n_hidden_2, convkernel, activation='relu', padding='same')(x)     # (256, 256)
-            x = tf.keras.layers.MaxPooling2D(poolkernel, padding='same')(x)                              # (128, 128)
-            x = tf.keras.layers.Conv2D(n_hidden_3, convkernel, activation='relu', padding='same')(x)     # (128, 128)
+            input = keras.layers.Input(shape=shape_img) # (512, 512)
+            x = keras.layers.Conv2D(n_hidden_1, convkernel, activation='relu', padding='same')(input) # (512, 512)
+            x = keras.layers.MaxPooling2D(poolkernel, padding='same')(x)                              # (256, 256)
+            x = keras.layers.Conv2D(n_hidden_2, convkernel, activation='relu', padding='same')(x)     # (256, 256)
+            x = keras.layers.MaxPooling2D(poolkernel, padding='same')(x)                              # (128, 128)
+            x = keras.layers.Conv2D(n_hidden_3, convkernel, activation='relu', padding='same')(x)     # (128, 128)
             encoded = tf.keras.layers.MaxPooling2D(poolkernel, padding='same')(x)                        # (64, 64)
 
-            x = tf.keras.layers.Conv2D(n_hidden_3, convkernel, activation='relu', padding='same')(encoded) # (64, 64)
-            x = tf.keras.layers.UpSampling2D(poolkernel)(x)                                                # (128, 128)
-            x = tf.keras.layers.Conv2D(n_hidden_2, convkernel, activation='relu', padding='same')(x)       # (128, 128)
-            x = tf.keras.layers.UpSampling2D(poolkernel)(x)                                                # (256,256)
-            x = tf.keras.layers.Conv2D(n_hidden_1, convkernel, activation='relu',padding="same")(x)        # (254,254)
-            x = tf.keras.layers.UpSampling2D(poolkernel)(x)
-            decoded = tf.keras.layers.Conv2D(shape_img[2], convkernel, activation='sigmoid', padding='same')(x)
+            x = keras.layers.Conv2D(n_hidden_3, convkernel, activation='relu', padding='same')(encoded) # (64, 64)
+            x = keras.layers.UpSampling2D(poolkernel)(x)                                                # (128, 128)
+            x = keras.layers.Conv2D(n_hidden_2, convkernel, activation='relu', padding='same')(x)       # (128, 128)
+            x = keras.layers.UpSampling2D(poolkernel)(x)                                                # (256,256)
+            x = keras.layers.Conv2D(n_hidden_1, convkernel, activation='relu',padding="same")(x)        # (254,254)
+            x = keras.layers.UpSampling2D(poolkernel)(x)
+            decoded = keras.layers.Conv2D(shape_img[2], convkernel, activation='sigmoid', padding='same')(x)
 
         else:
             raise Exception("Invalid model name given!")
 
         # Create autoencoder model
-        autoencoder = tf.keras.Model(input, decoded)
+        autoencoder = keras.Model(input, decoded)
         input_autoencoder_shape = autoencoder.layers[0].input_shape[1:]
         output_autoencoder_shape = autoencoder.layers[-1].output_shape[1:]
 
         # Create encoder model
-        encoder = tf.keras.Model(input, encoded)  # set encoder
+        encoder = keras.Model(input, encoded)  # set encoder
         input_encoder_shape = encoder.layers[0].input_shape[1:]
         output_encoder_shape = encoder.layers[-1].output_shape[1:]
 
         # Create decoder model
-        decoded_input = tf.keras.Input(shape=output_encoder_shape)
+        decoded_input = keras.Input(shape=output_encoder_shape)
         if self.modelName == 'simpleAE':
             decoded_output = autoencoder.layers[-1](decoded_input)  # single layer
         elif self.modelName == 'convAE':
@@ -93,7 +94,7 @@ class AutoEncoder():
             decoded_output = autoencoder.layers[-1](decoded_output)  # Conv2D
         else:
             raise Exception("Invalid model name given!")
-        decoder = tf.keras.Model(decoded_input, decoded_output)
+        decoder = keras.Model(decoded_input, decoded_output)
         decoder_input_shape = decoder.layers[0].input_shape[1:]
         decoder_output_shape = decoder.layers[-1].output_shape[1:]
 
@@ -117,9 +118,9 @@ class AutoEncoder():
     # Load model architecture and weights
     def load_models(self, loss="binary_crossentropy", optimizer="adam"):
         print("Loading models...")
-        self.autoencoder = tf.keras.models.load_model(self.info["autoencoderFile"])
-        self.encoder = tf.keras.models.load_model(self.info["encoderFile"])
-        self.decoder = tf.keras.models.load_model(self.info["decoderFile"])
+        self.autoencoder = keras.models.load_model(self.info["autoencoderFile"])
+        self.encoder = keras.models.load_model(self.info["encoderFile"])
+        self.decoder = keras.models.load_model(self.info["decoderFile"])
         self.autoencoder.compile(optimizer=optimizer, loss=loss)
         self.encoder.compile(optimizer=optimizer, loss=loss)
         self.decoder.compile(optimizer=optimizer, loss=loss)
